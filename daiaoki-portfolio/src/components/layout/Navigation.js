f256b3911ce9272c5bc72af6c57d0666d756c4ac
DaiAoki <a.dai.0814ap@gmail.com>
2018-11-15 15:52:54 -0800

    complete navigation

--- daiaoki-portfolio/src/components/layout/Navigation.js
+++ daiaoki-portfolio/src/components/layout/Navigation.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import { NavLink, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import COLORs from '../../shared/colors'
 import NUMBERs from '../../shared/number'
+import sword from '../../assets/images/sword.svg'
 
 const StyledNavigation = styled.nav`
   position: absolute;
@@ -18,36 +20,66 @@ const NavigationInner = styled.div`
   border: solid 4px #fff;
   border-radius: 4px;
 `
-const NavigationList = styled.ul`
+const NavigationList = styled.div`
   display: flex;
   flex-wrap: wrap;
-  padding: ${NUMBERs.MAGICK_NUMBER} calc(${NUMBERs.MAGICK_NUMBER} * 4);
+  padding: ${NUMBERs.MAGICK_NUMBER} 0;
+  text-align: center;
 `
-const NavigationItem = styled.li`
+const NavigationItem = styled(NavLink)`
   flex-basis: 50%;
   width: 50%;
   margin-bottom: ${NUMBERs.MAGICK_NUMBER};
-  &:nth-child(2n) {
-    text-align: right;
-  }
+  position: relative;
   &:nth-child(7), &:nth-child(8) {
     margin-bottom: 0;
   }
+  &:hover {
+    color: yellow;
+  }
+  &.active {
+    color: yellow;
+    img {
+      display: block;
+    }
+  }
+`
+const CurrentMark = styled.img`
+  display: none;
+  position: absolute;
+  top: 50%;
+  left: 6px;
+  transform: translateY(-50%) rotate(45deg);
+  width: 26px;
+  height: 26px;
 `
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      navItems: [
+        { name: 'はなす', path: '/contact'   }, { name: 'じゅもん', path: '/omajinai' },
+        { name: 'どうぐ', path: '/item'      }, { name: 'せんれき', path: '/history'  },
+        { name: 'そうび', path: '/equipment' }, { name: 'しらべる', path: '/search'   },
+        { name: 'つよさ', path: '/status'    }, { name: 'さくせん', path: '/tactics'  },
+      ]
+    }
+  }
+
   render() {
+    const navigationItems = this.state.navItems.map((item) => {
+      return (
+        <NavigationItem to={item.path}>
+          <CurrentMark src={sword}/>
+          { item.name }
+        </NavigationItem>
+      )
+    })
     return (
       <StyledNavigation>
         <NavigationInner>
           <NavigationList>
-            <NavigationItem>はなす</NavigationItem>
-            <NavigationItem>じゅもん</NavigationItem>
-            <NavigationItem>どうぐ</NavigationItem>
-            <NavigationItem>せんれき</NavigationItem>
-            <NavigationItem>そうび</NavigationItem>
-            <NavigationItem>しらべる</NavigationItem>
-            <NavigationItem>つよさ</NavigationItem>
-            <NavigationItem>さくせん</NavigationItem>
+            { navigationItems }
           </NavigationList>
         </NavigationInner>
       </StyledNavigation>
