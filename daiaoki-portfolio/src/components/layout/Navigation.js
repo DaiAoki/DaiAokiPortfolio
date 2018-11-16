2a68334a6c385cfa3893d4a2c366f8e6a302b3f1
DaiAoki <a.dai.0814ap@gmail.com>
2018-11-15 17:03:38 -0800

    implement experience timer

--- daiaoki-portfolio/src/components/layout/Navigation.js
+++ daiaoki-portfolio/src/components/layout/Navigation.js
@@ -53,6 +53,20 @@ const CurrentMark = styled.img`
   width: 26px;
   height: 26px;
 `
+const TotalPlayingTime = styled.div`
+  position: absolute;
+  top: ${NUMBERs.MAGICK_NUMBER};
+  right: ${NUMBERs.MAGICK_NUMBER};
+  background-color: ${COLORs.BASE_COLOR};
+  border-radius: 4px;
+  padding: 2px;
+  opacity: 0.9;
+`
+const TotalPlayingTimeInner = styled.div`
+  border: solid 4px #fff;
+  border-radius: 4px;
+  padding: ${NUMBERs.MAGICK_NUMBER};
+`
 class Navigation extends React.Component {
   constructor(props) {
     super(props)
@@ -62,27 +76,51 @@ class Navigation extends React.Component {
         { name: 'どうぐ', path: '/item'      }, { name: 'せんれき', path: '/history'  },
         { name: 'そうび', path: '/equipment' }, { name: 'しらべる', path: '/search'   },
         { name: 'つよさ', path: '/status'    }, { name: 'さくせん', path: '/tactics'  },
-      ]
+      ],
+      totalTime: ''
     }
+  }
+
+  componentWillMount() {
+    const componentThis = this
+    var zeroFill = function(num) {
+      return num < 10 ? `0${num}` : num
+    }
+    setInterval(function() {
+      var diff = (new Date().getTime()) - (new Date("2015/9/1").getTime())
+      var nowYear  = Math.floor(diff / NUMBERs.YEAR_NUM)
+      var nowMonth = Math.floor((diff % NUMBERs.YEAR_NUM)  / NUMBERs.MONTH_NUM)
+      var nowDay   = Math.floor((diff % NUMBERs.MONTH_NUM) / NUMBERs.DAY_NUM)
+      var nowHour  = Math.floor((diff % NUMBERs.DAY_NUM)   / NUMBERs.HOUR_NUM)
+      var nowMin   = zeroFill(Math.floor((diff % NUMBERs.HOUR_NUM) / NUMBERs.MIN_NUM))
+      var nowSec   = zeroFill(Math.floor((diff % NUMBERs.MIN_NUM)  / NUMBERs.SEC_NUM))
+      var total = `${nowYear}年${nowMonth}ヶ月${nowDay}日${nowHour}時間${nowMin}分${nowSec}秒`
+      componentThis.setState({ totalTime: total })
+    }, 1000)
   }
 
   render() {
     const navigationItems = this.state.navItems.map((item) => {
       return (
-        <NavigationItem to={item.path}>
+        <NavigationItem key={`navigationItem_${item.path}`} to={item.path}>
           <CurrentMark src={sword}/>
           { item.name }
         </NavigationItem>
       )
     })
     return (
-      <StyledNavigation>
-        <NavigationInner>
-          <NavigationList>
-            { navigationItems }
-          </NavigationList>
-        </NavigationInner>
-      </StyledNavigation>
+      <React.Fragment>
+        <StyledNavigation>
+          <NavigationInner>
+            <NavigationList>
+              { navigationItems }
+            </NavigationList>
+          </NavigationInner>
+        </StyledNavigation>
+        <TotalPlayingTime>
+          <TotalPlayingTimeInner>{`総プレイ時間： ${this.state.totalTime}`}</TotalPlayingTimeInner>
+        </TotalPlayingTime>
+      </React.Fragment>
     )
   }
 }
--- daiaoki-portfolio/src/shared/number.js
+++ daiaoki-portfolio/src/shared/number.js
@@ -1,4 +1,10 @@
 const NUMBERs = {
   MAGICK_NUMBER: '1.3125rem',
+  YEAR_NUM:  1000 * 60 * 60 * 24 * 30 * 12,
+  MONTH_NUM: 1000 * 60 * 60 * 24 * 30,
+  DAY_NUM:   1000 * 60 * 60 * 24,
+  HOUR_NUM:  1000 * 60 * 60,
+  MIN_NUM:   1000 * 60,
+  SEC_NUM:   1000,
 }
 export default NUMBERs
