ca9804d8cf2083da16411b92debfa0bd1c646253
DaiAoki <a.dai.0814ap@gmail.com>
2018-11-16 15:48:00 -0800

    omajinai page

--- daiaoki-portfolio/src/components/Omajinai.js
+++ daiaoki-portfolio/src/components/Omajinai.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
+import CenteredContainer from './general/CenteredContainer'
 import COLORs from '../shared/colors'
 import { media } from '../shared/media'
+import ANIMATIONs from '../shared/animations'
 
+const NoMore = styled.p`
+  font-size: 8.4rem;
+  text-shadow: 5px 5px 8px black;
+  animation: ${ANIMATIONs.NOMORE_ONE} 3.0s linear;
+  &:nth-child(2) {
+    transform: translateY(-100%);
+    animation: ${ANIMATIONs.NOMORE_TWO} 3.0s linear;
+  }
+`
 class Omajinai extends React.Component {
   render() {
     return (
-      <div>
-        hoge
-      </div>
+      <CenteredContainer>
+        <div>
+          <NoMore>No More おまじない!!!</NoMore>
+          <NoMore>No More おまじない!!!</NoMore>
+        </div>
+      </CenteredContainer>
     )
   }
 }
--- daiaoki-portfolio/src/shared/animations.js
+++ daiaoki-portfolio/src/shared/animations.js
@@ -2,10 +2,16 @@ import { keyframes } from 'styled-components'
 
 const ANIMATIONs = {
   FLASHING: keyframes`
-    0% {opacity: 1} 40% {opacity: 0} 43% {opacity: 1} 55% {opacity: 1} 70% {opacity: 0.1} 73% {opacity: 1} 76% {opacity: 0.1} 79% {opacity: 1} 100% {opacity: 1}
+    0%{opacity: 1} 40%{opacity: 0} 43%{opacity: 1} 55%{opacity: 1} 70%{opacity: 0.1} 73%{opacity: 1} 76%{opacity: 0.1} 79%{opacity: 1} 100%{opacity: 1}
   `,
   MONSTER: keyframes`
-    0% {transform: translateY(0)} 15% {transform: translateY(0)} 27% {transform: translateY(-10%) scale(1.1)} 34% {transform: translateY(0)} 46% {transform: translateY(-10%) scale(1.1)} 53% {transform: translateY(0)} 60% {transform: translateY(0)} 72% {transform: translateY(-20%) scale(1.1)} 80% {transform: translateY(0)} 100% {transform: translateY(0)}
+    0%{transform: translateY(0)} 15%{transform: translateY(0)} 27%{transform: translateY(-10%) scale(1.1)} 34%{transform: translateY(0)} 46%{transform: translateY(-10%) scale(1.1)} 53%{transform: translateY(0)} 60%{transform: translateY(0)} 72%{transform: translateY(-20%) scale(1.1)} 80%{transform: translateY(0)} 100%{transform: translateY(0)}
+  `,
+  NOMORE_ONE: keyframes`
+    0%{transform: rotate(50deg)} 10%{transform: rotate(-40deg)} 14%{transform: rotate(-35deg)} 20%{transform: rotate(30deg)} 23%{transform: rotate(20deg)} 26%{transform: rotate(-15deg)} 30%{transform: rotate(5deg)} 32%{transform: rotate(0deg)} 100%{transform: rotate(0deg)}
+  `,
+  NOMORE_TWO: keyframes`
+    0%{transform: translateY(-100%) rotate(-50deg)} 10%{transform: translateY(-100%) rotate(40deg)} 14%{transform: translateY(-100%) rotate(35deg)} 20%{transform: translateY(-100%) rotate(-30deg)} 23%{transform: translateY(-100%) rotate(-20deg)} 26%{transform: translateY(-100%) rotate(15deg)} 30%{transform: translateY(-100%) rotate(-5deg)} 32%{transform: translateY(-100%) rotate(0deg)} 100%{transform: translateY(-100%) rotate(0deg)}
   `,
 }
 export default ANIMATIONs
